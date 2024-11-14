43d369ccce50fe4f295a4b024346e2dcf7e3c426
yujin-lim <limu88@naver.com>
2024-11-14 18:23:29 +0900

    message

--- src/testrpg/StageSetting.java
+++ src/testrpg/StageSetting.java
@@ -2,6 +2,7 @@ package testrpg;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
@@ -22,6 +23,13 @@ public class StageSetting extends Stage {
 			buffer.append("[4.인벤토리]");
 			buffer.append("[5.파일]");
 			buffer.append("[0.종료]");
+			
+			try {
+				String input = reader.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
 			
 		}
